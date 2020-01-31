0dbd227eff79c32b0b6c6d08686abc8edacde571
Eian Carter <eian@bricleir.com>
2020-01-31 09:05:47 -0800

    built out login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
-const Login = () => {
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    setIsLoading(true);
+    axiosWithAuth()
+      .post('/api/login', credentials)
+      .then(res => {
+        setIsLoading(false)
+        console.log('authentication response', res)
+        localStorage.setItem('token', res.data.payload)
+        history.push('/bubble-page');
+      })
+      .catch(err => console.log(err));
+  }
+
+  const handleChange = e => {
+    setCredentials({...credentials, [e.target.name]: e.target.value })
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={submitHandler}>
+        <input 
+          type='text'
+          name='username'
+          onChange={handleChange}
+          placeholder='username'
+          value={credentials.username}
+          required
+        />
+        <input 
+          type='password'
+          name='password'
+          onChange={handleChange}
+          placeholder='password'
+          value={credentials.password}
+          required
+        />
+        {
+          !!isLoading && <div>loading...</div>
+        }
+        <button>Log In</button>
+      </form>
     </>
   );
 };
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import axios from 'axios';
 
 const axiosWithAuth = () => {
     return axios.create({
         baseURL: 'http://localhost:5000',
         headers: {
-            Authorization: localStorage.getItem('token')
+            authorization: localStorage.getItem('token')
         }
     })
 }
